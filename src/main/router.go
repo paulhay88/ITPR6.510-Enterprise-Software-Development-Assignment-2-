e2d94431138225bddeb2b69e882a505fdd47ca23
catokass <kassian.houben@gmail.com>
2018-10-02 12:34:51 +1300

    Add output function.

--- src/main/router.go
+++ src/main/router.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -25,14 +25,6 @@ func getMeetings(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	var myMeetings Meetings
 
-	// var myMeetings = Meetings{[]Meeting{Meeting{1, time.Now(), 1, "topic", "agenda", 1}}}
-
-	// var myMeetings = Meeting{1, time.Now(), 1, "topic", "agenda", 1}
-
-	// b, err := json.MarshalIndent(myMeetings, "", "\t")
-	// check(err)
-	// fmt.Fprintf(w, string(b))
-
 	meetings, err := meetingplannerdb.Query(`SELECT * FROM meetings`)
 	check(err)
 
@@ -42,23 +34,22 @@ func getMeetings(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	for meetings.Next() {
 
-		var (
-			id          int
-			topic       string
-			dateAndTime time.Time
-			agenda      string
-			roomID      int
-			ownerID     int
-		)
+		// Create new meeting. Used to store scanned query information.
+		// Is appended to meetings at the end and outputed.
 
-		// err := meetings.Scan(&id, &topic, &dateAndTime, &agenda, &roomID, &ownerID)
-		// check(err)
+		var meeting Meeting
 
-		err := meetings.Scan(&id, &topic, &dateAndTime, &agenda, &roomID, &ownerID)
+		err := meetings.Scan(&meeting.ID, &meeting.Topic, &meeting.TimeAndDate, &meeting.Agenda, &meeting.RoomID, &meeting.OwnerID)
 		check(err)
 
-		// result := strings.Join([]string{strconv.Itoa(id), topic, dateAndTime.String(), agenda, strconv.Itoa(roomID), strconv.Itoa(ownerID)}, " ") + "\n"
-		// fmt.Fprintf(w, result)
-		// w.Write([]byte(result))
+		myMeetings.Meetings = append(myMeetings.Meetings, meeting)
 	}
+
+	output(w, myMeetings)
+}
+
+func output(w http.ResponseWriter, myMeetings interface{}) {
+	b, err := json.MarshalIndent(myMeetings, "", "\t")
+	check(err)
+	fmt.Fprintf(w, string(b))
 }
