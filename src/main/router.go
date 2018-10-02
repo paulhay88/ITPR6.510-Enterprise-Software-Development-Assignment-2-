8201455f44f66decb7bb4bc8b564557204e5742c
paulhay88 <paulhay88@gmail.com>
2018-10-02 14:43:15 +1300

    frm paul

--- src/main/router.go
+++ src/main/router.go
@@ -11,20 +11,12 @@ import (
 
 func route() {
 	router := httprouter.New()
+	// router.Handle("/", getHandler).Methods("GET")
+	// router.Handle("/", postHandler).Methods("POST")
+	// router.Handle("/", puthandler).Methods("PUT")
+	// router.handle("/", deletehandle).Methods("DELETE")
 	router.GET("/", getMeetings)
-	router.POST("/", createMeeting)
-	router.PUT("/meeting/edit/:id", updateMeeting)
-
-	// Testing
-	router.POST("/outputInput", outputInput)
 	log.Fatal(http.ListenAndServe(":9090", router))
-}
-
-// Turn struct
-func output(w http.ResponseWriter, myStruct interface{}) {
-	b, err := json.MarshalIndent(myStruct, "", "\t")
-	check(err)
-	fmt.Fprintf(w, string(b))
 }
 
 // Any Routing functions...
@@ -56,47 +48,9 @@ func getMeetings(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	output(w, myMeetings)
 }
 
-func createMeeting(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-
-	var meeting = new(Meeting)
-
-	err := json.NewDecoder(r.Body).Decode(&meeting)
+func output(w http.ResponseWriter, myMeetings interface{}) {
+	b, err := json.MarshalIndent(myMeetings, "", "\t")
 	check(err)
-
-	_, err = meetingplannerdb.Exec(
-		`INSERT INTO meetings(dateAndTime, roomID, topic, agenda, ownerID) VALUES($1, $2, $3, $4, $5)`,
-		meeting.TimeAndDate, meeting.RoomID, meeting.Topic, meeting.Agenda, meeting.OwnerID)
-
-	check(err)
+	fmt.Fprintf(w, string(b))
 }
 
-func updateMeeting(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-
-	var meeting = new(Meeting)
-
-	err := json.NewDecoder(r.Body).Decode(&meeting)
-	check(err)
-
-	_, err = meetingplannerdb.Exec(
-		`UPDATE meetings
-		 SET roomID = $1, 
-			 topic = $2, 
-			 agenda = $3, 
-			 ownerID = $4,
-			 dateAndTime = $5
-		 WHERE 
-		 	 id = $6`,
-		meeting.RoomID, meeting.Topic, meeting.Agenda, meeting.OwnerID, meeting.TimeAndDate, params.ByName("id"))
-	check(err)
-}
-
-// Takes any input and outputs it. For testing purposes.
-func outputInput(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-
-	var inter interface{}
-
-	err := json.NewDecoder(r.Body).Decode(&inter)
-	check(err)
-
-	output(w, inter)
-}
