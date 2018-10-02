10d112c45d30ae22e4cb399bc75453daf1a4349b
catokass <kassian.houben@gmail.com>
2018-10-02 14:44:54 +1300

    Add meetings routing.

--- src/main/router.go
+++ src/main/router.go
@@ -50,6 +50,30 @@ func getMeetings(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		err := meetings.Scan(&meeting.ID, &meeting.Topic, &meeting.TimeAndDate, &meeting.Agenda, &meeting.RoomID, &meeting.OwnerID)
 		check(err)
 
+		// Get participants belonging to meeting
+
+		var participants Participants
+
+		participants, err = meetingplannerdb.Query(`SELECT * FROM participants WHERE meetingID=$1`, meeting.ID)
+		check(err)
+
+		defer participants.Close()
+
+		fmt.Fprintf(w, "\nParticipants: \n")
+
+		for meetings.Next() {
+
+			// Create new meeting. Used to store scanned query information.
+			// Is appended to meetings at the end and outputed.
+
+			var meeting Meeting
+
+			err := meetings.Scan(&meeting.ID, &meeting.Topic, &meeting.TimeAndDate, &meeting.Agenda, &meeting.RoomID, &meeting.OwnerID)
+			check(err)
+
+			myMeetings.Meetings = append(myMeetings.Meetings, meeting)
+		}
+
 		myMeetings.Meetings = append(myMeetings.Meetings, meeting)
 	}
 
