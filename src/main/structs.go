ff99f56faf6d0a64f7f0db0a493470ff9659a771
paulhay88 <paulhay88@gmail.com>
2018-10-03 13:16:36 +1300

    frm paul

--- src/main/structs.go
+++ src/main/structs.go
@@ -43,20 +43,21 @@ type Meetings struct {
 	Meetings []Meeting `json:"meetings"`
 }
 type Meeting struct {
-	ID           int       `json:"id"`
-	TimeAndDate  time.Time `json:"timeAndDate"`
-	RoomID       int       `json:"roomID"`
-	Topic        string    `json:"topic"`
-	Agenda       string    `json:"agenda"`
-	OwnerID      int       `json:"ownerID"`
-	Participants []User
+	ID          int       `json:"id"`
+	TimeAndDate time.Time `json:"timeAndDate"`
+	RoomID      int       `json:"roomID"`
+	Topic       string    `json:"topic"`
+	Agenda      string    `json:"agenda"`
+	OwnerID     int       `json:"ownerID"`
 }
 
-type Participants struct {
+type Partipants struct {
 	Partipants []Participant `json:"participants"`
 }
 type Participant struct {
-	ID        int `json:"id"`
-	MeetingID int `json:"meetingID"`
-	UserID    int `json:"userID"`
+	Name      string `json:"name"`
+	MeetingID int    `json:"meetingID"`
+	UserID    int    `json:"userID"`
+	OwnerID   int    `json:"owner"`
 }
+
