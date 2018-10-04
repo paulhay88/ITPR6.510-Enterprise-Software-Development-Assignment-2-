e2e98e82c2017d4c7bc2f96a20b48568b97a5ec3
paulhay88 <paulhay88@gmail.com>
2018-10-04 13:08:25 +1300

    frm paul Login + cookies

--- src/main/login.go
+++ src/main/login.go
@@ -44,7 +44,46 @@ func routeLog() {
 			check(err)
 			//coompare 
 			if user.Name == logInUser.userName && user.Password == logInUser.password{
-				//validate
+				/*
+				validatation here and proceed to create cookie
+				setting a cookie:
+				*/
+				expiration := time.Now().Add(365 * 24 * time.Hour)
+				cookie := http.Cookie{Name: user.Name, Value: user.Password, Expires: expiration, Secure: true}
+				http.SetCookie(w, &cookie)
+				message = "User Accepted"
+				http.Redirect(w, r, "/", http.StatusFound)
+			}
+			else{
+				message = "User Not accepted"
+				http.Redirect(w, r, "/login/", http.StatusFound)
 			}
 		}
 	}
+
+	//COOKIES
+	/*
+	http.SetCookie(w ResponseWriter, cookie *Cookie)
+	type Cookie struct {
+	    Name       string
+	    Value      string
+	    Path       string
+	    Domain     string
+	    Expires    time.Time
+	    RawExpires string
+	    MaxAge   int
+	    Secure   bool
+	    HttpOnly bool
+	    Raw      string
+	    Unparsed []string // Raw text of unparsed attribute-value pairs
+	}
+	*/
+//get a cookie that has been set
+//cookie, _ := r.Cookie("username")
+//fmt.Fprint(w, cookie)
+	//Here is another way to get a cookie
+	/*
+	for _, cookie := range r.Cookies() {
+	    fmt.Fprint(w, cookie.Name)
+	}
+	*/
