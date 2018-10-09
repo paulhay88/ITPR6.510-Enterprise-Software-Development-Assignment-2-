bb92746aa9afc72a848227cee048826deaf8f4f7
paulhay88 <paulhay88@gmail.com>
2018-10-09 21:33:22 +1300

    frm paul checks and errNoRows for Queries

--- src/main/findMeetings.go
+++ src/main/findMeetings.go
@@ -7,10 +7,15 @@ import (
 
 func findOwnedMeeting(w http.ResponseWriter, r *http.Request, httprouter.Params) {
 	var findMeeting = new(Meeting)
-	err := json.NewDecoder(r.Body).Decode(&findMeeting)
+	defer findMeeting.Close()
+	decode := json.NewDecoder(r.Body).Decode(&findMeeting)
+	check(decode)
+	OwnedMeeting, err := meetingplannerdb.QueryRow(`SELECT * FROM meetings WHERE ownerID=$1`, user.UserID)
 	check(err)
-	OwnedMeeting := meetingplannerdb.QueryRow(`SELECT * FROM meetings WHERE ownerID=$1`, user.UserID)
-	result := foundMeeting.Scan(&id, &dateAndTime, &roomID, &topic, &agenda, &ownerID, &participants)
+	defer OwnedMeeting.Close()
+	result, err2 := foundMeeting.Scan(&id, &dateAndTime, &roomID, &topic, &agenda, &ownerID, &participants)
+	defer result.Close()
+	check(err2)
 	if result == sql.ErrNoRows {
 		output(w, "No Data :")
 	} else {
@@ -21,36 +26,69 @@ func findOwnedMeeting(w http.ResponseWriter, r *http.Request, httprouter.Params)
 
 func findMyParticipantMeetings(w http.ResponseWriter, r *http.Request, httprouter.Params){
 	//Finds meeting by userName from Cookie
-	meetingCookie, _ := r.Cokkie("authUser")
-	userName, err := strings.Split(meetingCookie.Value, ":")[0]
+	meetingCookie, err := r.Cokkie("authUser")
 	check(err)
+	defer meetingCookie.Close()
+	userName, err2 := strings.Split(meetingCookie.Value, ":")[0]
+	defer userName.Close()
+	check(err2)
 	//For all meetings in DB
-	ParticipantMeeting := meetingplannerdb.QueryRow(`SELECT * FROM meetings WHERE participants = $1`, userName)
-	if result == sql.ErrNoRows {
+	participantMeeting, err3 := meetingplannerdb.QueryRow(`SELECT * FROM meetings WHERE participants = $1`, userName)
+	defer ParticipantMeeting.Close()
+	check(err3)
+	if participantMeeting == sql.ErrNoRows {
 		output(w, "No Data :")
 	} else {
-		fmt.Println("Owner of :\n")
-		output(w, ParticipantMeeting)
+		fmt.Println("Participant of Meeting:\n")
+		output(w, participantMeeting)
 	}
 }
 
-func FindRoom(w http.ResponseWriter, r *http.Request, s httprouter.Params){
+func FindRoom(w http.ResponseWriter, r *http.Request, httprouter.Params){
 	//need to know how to reference the info user puts in what variable to call and run through RegEx expression
-	roomNumber := meetingplannerdb.QueryRow(`SELECT * FROM meetings WHERE RoomID = $1`, s)  //variable based on input from user RegEx
-	output(w, roomNumber)
+	reg, err := regexp.MustCompile((\d+)([0-9]+))
+	check(err)
+	defer reg.Close()
+	roomNumber, err2 := meetingplannerdb.QueryRow(`SELECT * FROM meetings WHERE RoomID = $1`, reg)  //variable based on input from user RegEx
+	defer roomNumber.Close()
+	check(err2)
+	if roomNumber == sql.ErrNoRows {
+		output(w, "No Data :")
+	} else {
+		fmt.Println("Room Number:\n")
+		output(w, roomNumber)
+	}
 }
 
-func AgendaSearch(w http.ResponseWriter, r *http.Request, s httprouter.Params){ //using s as the string to be used within the regular expression
-	reg := regexp.MustCompile([^.?!]*(?<=[.?\s!])string(?=[\s.?!])[^.?!]*[.?!])
-	agendaReturn := meetingplannerdb.QueryRow(`SELECT * FROM meetings WHERE Agenda = $1`, reg)
-	output(w, agendaReturn)
+func AgendaSearch(w http.ResponseWriter, r *http.Request, httprouter.Params){ //using s as the string to be used within the regular expression
+	reg, err := regexp.MustCompile([^.?!]*(?<=[.?\s!])string(?=[\s.?!])[^.?!]*[.?!])
+	defer reg.Close()
+	check(err)
+	agendaReturn, err2 := meetingplannerdb.QueryRow(`SELECT * FROM meetings WHERE Agenda = $1`, reg)
+	defer agendaReturn.Close()
+	check(err2)
+	if agendaReturn == sql.ErrNoRows {
+		output(w, "No Data :")
+	} else {
+		fmt.Println("Agenda Return :\n")
+		output(w, agendaReturn)
+	}
 }
 
 
 func TopicSearch(w http.ResponseWriter, r *http.Request, s httprouter.Params){ //using s as the string to be used within the regular expression
-	reg := regexp.MustCompile([a-zA-Z0-9])
-	topicReturn := meetingplannerdb.QueryRow(`SELECT * FROM meetings WHERE Agenda = $1`, reg)
-	output(w, topicReturn)
+	reg, err := regexp.MustCompile([a-zA-Z0-9])
+	defer reg.Close()
+	check(err)
+	topicReturn, err2 := meetingplannerdb.QueryRow(`SELECT * FROM meetings WHERE Agenda = $1`, reg)
+	defer topicReturn.Close()
+	check(err2)
+	if topicReturn == sql.ErrNoRows {
+		output(w, "No Data :")
+	} else {
+		fmt.Println("Topic Return:\n")
+		output(w, topicReturn)
+	}
 }
 
 /*
