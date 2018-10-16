8f9c8a5cdc929b8fd53c5b44d11adbf9225208e9
paulhay88 <paulhay88@gmail.com>
2018-10-16 15:12:50 +1300

    frm paul Agenda search router work/testing

--- src/main/agendaSearch.go
+++ src/main/agendaSearch.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 	"strings"
 
@@ -9,19 +8,60 @@ import (
 )
 
 func agendaSearch(w http.ResponseWriter, r *http.Request, Params httprouter.Params) {
+	//var meetings Meetings
+	var values []interface{}
+
+	var counter int
 	agendaCookie, err := r.Cookie("authUser")
 	check(err)
-	//getting cokkie
-	output(w, "test1")
-
-	var se = new(agendaSearchStruct)
-	//getting username from cookie
 	userName := strings.Split(agendaCookie.Value, ":")[0]
-	//decoding the body aka search field
-	err = json.NewDecoder(r.Body).Decode(&se)
-	check(err)
-	//full query using contians
-	searchQuery := meetingplannerdb.QueryRow(`SELECT * FROM meetings WHERE userName=$1 AND agenda CONTAINS=$2`, userName, se)
-	output(w, searchQuery)
+	userID := meetingplannerdb.QueryRow(`SELECT id FROM users WHERE userName=$1`, userName)
 
+	for _, k := range []string{"sentence", "phoneNumbe", "email", "keyWords", "dollar"} {
+		if v, err := r.URL.Query()[k]; err {
+			counter++
+			var value interface{}
+
+			if k == "sentence" {
+				output(w, v[0])
+				//x = v[0]
+				//y := meetingplannerdb.Query("SELECT regexp_matches() FROM meetings WHERE id=$1", userID)
+			} else if k == "phoneNumbe" {
+				output(w, v[0])
+			} else if k == "email" {
+				output(w, v[0])
+			} else if k == "keyWords" {
+				output(w, v[0])
+			} else if k == "dollar" {
+				output(w, v[0])
+			} else {
+				value = v[0]
+			}
+			values = append(values, value)
+
+		}
+		if len(values) == 0 {
+			http.Redirect(w, r, "/", http.StatusFound)
+			return
+		}
+
+	}
+	output(w, userID)
+	//output(w, r.URL.Query())
+	/*
+		agendaCookie, err := r.Cookie("authUser")
+		check(err)
+		//getting cokkie
+		output(w, "test1")
+
+		var se = new(agendaSearchStruct)
+		//getting username from cookie
+		userName := strings.Split(agendaCookie.Value, ":")[0]
+		//decoding the body aka search field
+		err = json.NewDecoder(r.Body).Decode(&se)
+		check(err)
+		//full query using contians
+		searchQuery := meetingplannerdb.QueryRow(`SELECT * FROM meetings WHERE userName=$1 AND agenda CONTAINS=$2`, userName, se)
+		output(w, searchQuery)
+	*/
 }
