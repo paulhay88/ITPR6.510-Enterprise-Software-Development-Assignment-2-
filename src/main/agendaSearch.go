4aa3c4b590393deefa58e7c49f0b5cfddd2745e0
paulhay88 <paulhay88@gmail.com>
2018-10-23 20:09:14 +1300

    frm paul RegEx all but list of 3

--- src/main/agendaSearch.go
+++ src/main/agendaSearch.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"strings"
 
@@ -9,48 +8,77 @@ import (
 )
 
 func agendaSearch(w http.ResponseWriter, r *http.Request, Params httprouter.Params) {
-	//var meetings Meetings
 	var values []interface{}
-
+	var myMeetings Meetings
 	var counter int
-	//agendaCookie, err := r.Cookie("authUser")
-	//check(err)
-	//userName := strings.Split(agendaCookie.Value, ":")[0]
-	//userID := meetingplannerdb.QueryRow(`SELECT id FROM users WHERE userName=$1`, userName)
-	//var regExpression string
 	var searchString string
-	for _, k := range []string{"sentence", "phoneNumbe", "email", "keyWords", "dollar"} {
+	for _, k := range []string{"sentence", "phoneNumber", "email", "keyWords", "dollar"} {
 		if v, err := r.URL.Query()[k]; err {
 			counter++
 			var value interface{}
-
 			if k == "sentence" {
 				searchString = v[0]
-				// Creating the RegEx as a String using []strings to build full query
-				p1 := `(^\b)(`
+				p1 := `(^\b`
 				p2 := searchString
-				p3 := `)(\ )([a-zA-Z0-9\ \-]+)?(\.)?`
-				newA := []string{p1, p2, p3}
+				p3 := `)?`
+				p4 := `(\_[a-zA-Z0-9]+)?`
+				newA := []string{p1, p2, p3, p4}
 				AnotherOne := strings.Join(newA, "")
-				fmt.Fprintf(w, AnotherOne)
-				output(w, "test")
-				//------------
-				results, err := meetingplannerdb.Query("SELECT * FROM meetings WHERE agenda LIKE $1", AnotherOne)
+				results, err := meetingplannerdb.Query("SELECT * FROM meetings WHERE agenda ~* $1", AnotherOne)
 				check(err)
-				output(w, results)
-
-			} else if k == "phoneNumbe" {
-				output(w, v[0])
-				//regExpression = `([a-zA-Z\ \-\$\.]+)?(\ )?([0-9\ \-\.]+)([a-zA-Z\ \-\.]+)`
+				for results.Next() {
+					var meeting Meeting
+					err := results.Scan(&meeting.ID, &meeting.Topic, &meeting.TimeAndDate, &meeting.Agenda, &meeting.RoomID, &meeting.OwnerID)
+					check(err)
+					myMeetings.Meetings = append(myMeetings.Meetings, meeting)
+				}
+				output(w, myMeetings)
+			} else if k == "phoneNumber" {
+				searchString = v[1]
+				p0 := `([a-zA-Z0-9\s\.\-]+)?`
+				p1 := `(`
+				p2 := searchString
+				p3 := `)?`
+				p4 := `([a-zA-Z0-9\s\.\-]+)?`
+				newA := []string{p0, p1, p2, p3, p4}
+				AnotherOne := strings.Join(newA, "")
+				results, err := meetingplannerdb.Query("SELECT * FROM meetings WHERE agenda ~* $1", AnotherOne)
+				check(err)
+				for results.Next() {
+					var meeting Meeting
+					err := results.Scan(&meeting.ID, &meeting.Topic, &meeting.TimeAndDate, &meeting.Agenda, &meeting.RoomID, &meeting.OwnerID)
+					check(err)
+					myMeetings.Meetings = append(myMeetings.Meetings, meeting)
+				}
+				output(w, myMeetings)
 			} else if k == "email" {
-				output(w, v[0])
-				//regExpression = `([a-zA-Z\ \-]+)(\ )([a-zA-Z0-9\@]+)(\.)(com|net|us|ca|org|au|co\.nz)([a-zA-Z\ \-\.]+)`
+				searchString = v[1]
+				p0 := `([a-zA-Z0-9\s\-\.]+)(\s)`
+				p1 := `(`
+				p2 := searchString
+				p3 := `)?`
+				p4 := `(\@)?([a-zA-Z]+)?(\.)?(com|au|co.nz)?([a-zA-Z0-9\s\-\.]+)`
+				newA := []string{p0, p1, p2, p3, p4}
+				AnotherOne := strings.Join(newA, "")
+				results, err := meetingplannerdb.Query("SELECT * FROM meetings WHERE agenda ~* $1", AnotherOne)
+				check(err)
+				for results.Next() {
+					var meeting Meeting
+					err := results.Scan(&meeting.ID, &meeting.Topic, &meeting.TimeAndDate, &meeting.Agenda, &meeting.RoomID, &meeting.OwnerID)
+					check(err)
+					myMeetings.Meetings = append(myMeetings.Meetings, meeting)
+				}
+				output(w, myMeetings)
 			} else if k == "keyWords" {
 				output(w, v[0])
 				/*
-					var var1 = "string"
-					var var2 = "string"
-					var var3 = "string"
+					for len(v){
+						insert into array of strings
+						concatanate those and search for them
+					}
+						var var1 = "string"
+						var var2 = "string"
+						var var3 = "string"
 				*/
 				//regExpression = `([a-zA-Z\ \-\.\n]+)(\ )( ` + var1 + `)(\ )([a-zA-Z\ \-\.\n]+)(\ )(` + var2 + `)(\ )([a-zA-Z\ \-\.\n]+)(\ )(` + var3 + `)(\ )([a-zA-Z\ \-\.\n]+)`
 			} else if k == "dollar" {
